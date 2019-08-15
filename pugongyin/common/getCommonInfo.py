1e32bee760c3129d009e3f69510d44fc9061c62b
180331 <15158027703@163.com>
2019-08-15 16:39:19 +0800

    add

--- pugongyin/common/getCommonInfo.py
+++ pugongyin/common/getCommonInfo.py
@@ -10,7 +10,6 @@ url = readConfig.ReadConfig().get_http("gzhonlineurl")#获取配置文件中的u
 
 response = requests.get(url+'/api/login/detail?token=gzhlogin:0f289302-3cbd-4a5a-8e47-edd071a0a1d0')
 
-
 versionInfo = response.text
 
 versionInfoPython = json.loads(versionInfo)
@@ -29,10 +28,15 @@ WareDTOs = goodsList.get('WareDTOs')
 
 wareId = WareDTOs[0].get('Id')
 
+xcxtoken = 'xcxlogin%3A16e265ff-ff1f-4ab1-a998-1f6325c8a357&AppInfoCode=code001&MerchantId=645d5071-1c22-4161-a093-7566f5cca058'
+
 def getShipOwnerId():
     return shipOwnerId
 
 def getWareId():
     return wareId
+
+def getXcxToken():
+    return xcxtoken
 
 
